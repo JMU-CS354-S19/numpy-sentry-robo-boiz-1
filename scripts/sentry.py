3f061b46e6dd231ece0ab3e0bd3274e728db6d03
Butler <butlerbw@l200210.cs.jmu.edu>
2019-01-24 13:44:57 -0500

    Started on lab.

--- scripts/sentry.py
+++ scripts/sentry.py
@@ -32,6 +32,11 @@ class SentryNode(object):
         self.cv_bridge = CvBridge()
         rospy.Subscriber('/camera/depth_registered/image',
                          Image, self.depth_callback, queue_size=1)
+        self.c = None
+        self.p = None
+        self.avg = 1
+        self.alpha = .5
+        self.threshold = .5
         rospy.spin()
 
     def depth_callback(self, depth_msg):
@@ -42,7 +47,26 @@ class SentryNode(object):
 
         # YOUR CODE HERE.
         # HELPER METHODS ARE GOOD.
-
+        x, y = depth.shape
+        
+        if self.c is not None:
+            self.p = self.c
+            
+        self.c = depth[:, x/2] # extract central column
+        
+        self.c = self.c[~np.isnan(self.c)]
+        
+        if self.p is not None and self.c is not None:            
+            diff_arr = self.c - self.p
+            np.absolute(diff_arr)
+            d = np.sum(diff_arr)
+        
+            #d = 10
+            self.avg = self.avg * self.alpha + d * (1-self.alpha)
+        
+            if d/self.avg > self.threshold:
+                rospy.loginfo("reached")
+        #rospy.loginfo(depth[:, x/2])
 
 if __name__ == "__main__":
     SentryNode()
