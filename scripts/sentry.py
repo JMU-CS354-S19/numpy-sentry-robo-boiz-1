4ad8b6c9a3998ea4f9a0a83174444a64f2153044
Butler <butlerbw@l200210.cs.jmu.edu>
2019-01-31 15:40:09 -0500

    Fixed beep working now.

--- scripts/sentry.py
+++ scripts/sentry.py
@@ -9,6 +9,7 @@ Version:
 
 import rospy
 
+from kobuki_msgs.msg import Sound
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
@@ -35,8 +36,8 @@ class SentryNode(object):
         self.c = None
         self.p = None
         self.avg = 1
-        self.alpha = .5
-        self.threshold = .5
+        self.alpha = .7
+        self.threshold = 2
         rospy.spin()
 
     def depth_callback(self, depth_msg):
@@ -45,8 +46,6 @@ class SentryNode(object):
         # Convert the depth message to a numpy array
         depth = self.cv_bridge.imgmsg_to_cv2(depth_msg)
 
-        # YOUR CODE HERE.
-        # HELPER METHODS ARE GOOD.
         x, y = depth.shape
         
         if self.c is not None:
@@ -54,19 +53,18 @@ class SentryNode(object):
             
         self.c = depth[:, x/2] # extract central column
         
-        #self.c = self.c[~np.isnan(self.c)]
-        
         if self.p is not None and self.c is not None:            
             diff_arr = self.c - self.p
-            np.absolute(diff_arr)
-            d = np.nansum(diff_arr)
+            d = np.nansum(np.absolute(diff_arr))
         
             self.avg = self.avg * self.alpha + d * (1-self.alpha)
+            #rospy.loginfo(d/self.avg)
         
             if d/self.avg > self.threshold:
-                # pub = rospy.Publisher('/mobile_base/commands/sound', std_msgs.msg.String, queue_size=10)
-                # pub.publish('')
-                rospy.loginfo("reached")
+                pub = rospy.Publisher('/mobile_base/commands/sound', Sound, queue_size=10)
+                sound = Sound(0)
+                pub.publish(sound)
+
 
 if __name__ == "__main__":
     SentryNode()
